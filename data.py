00e096be1fdbbb85f3906e7d7ed1580023a86545
VarinCode <warinsaipanya471@outlook.co.th>
2023-10-10 01:01:08 +0700

    อัปเดตและเพิ่มฟีเจอร์ใหม่
    เพิ่มระบบ login ระบบ สมัครสมาชิก , เพิ่มสิทธิ์การใช้งานคำสั่ง , แก้ไข logic , เขียน comment อธิบาย code เพิ่มเติม , แก้ไขชื่อ method และ อื่นๆ

--- data.py
+++ data.py
@@ -1,19 +1,83 @@
 from typing import Dict
 
+# ข้อมูลผู้ใช้งานในโปรแแกรมนี้จะถูกเก็บไว้ใน list userData
+# ข้างในมี dictionary ของผู้ใช้งานแต่ละคน
+
 userData = [
     {
         "name": "john",
         "email": "john@email.com",
-        "password": "123456",
-        "position": "ผู้บริหาร"
+        "password": "111",
+        "position": "ผู้จัดการ",
+    },
+    {
+        "name": "david",
+        "email": "david@email.com",
+        "password": "222",
+        "position": "ผู้ดูแลระบบ",
     },
     {
         "name": "jane",
         "email": "jane@email.com",
-        "password": "000000",
-        "position": "แคชเชียร์"
-    }
+        "password": "333",
+        "position": "แคชเชียร์",
+    },
+    {
+        "name": "jack",
+        "email": "jack@email.com",
+        "password": "444",
+        "position": "แคชเชียร์",
+    },
+    {
+        "name": "elizabeth",
+        "email": "elizabeth@email.com",
+        "password": "555",
+        "position": "พนักงานบาร์",
+    },
+    {
+        "name": "robert",
+        "email": "robert@email.com",
+        "password": "666",
+        "position": "ซอมเมลิเยร์",
+    },
+    {
+        "name": "jill",
+        "email": "jill@email.com",
+        "password": "777",
+        "position": "ผู้จัดการครัว",
+    },
+    {
+        "name": "jame",
+        "email": "james@email.com",
+        "password": "888",
+        "position": "กุ๊ก",
+    },
+    {
+        "name": "william",
+        "email": "william@email.com",
+        "password": "999",
+        "position": "ผู้ช่วยกุ๊ก",
+    },
+    {
+        "name": "linda",
+        "email": "linda@email.com",
+        "password": "123",
+        "position": "บริกร",
+    },
+    {
+        "name": "jennifer",
+        "email": "jennifer@email.com",
+        "password": "456",
+        "position": "พนักงานต้อนรับ",
+    },
+    {
+        "name": "mary",
+        "email": "mary@email.com",
+        "password": "695",
+        "position": "พนักงานต้อนรับ",
+    },
 ]
 
-def addUser(newUser: Dict[str , str]) -> None:
-    userData.append(newUser)   
+# function ในการเพิ่มผู้ใช้งาน
+def addUser(newUser: Dict[str, str]) -> None:
+    userData.append(newUser)
