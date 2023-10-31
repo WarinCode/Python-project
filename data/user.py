02eddf74caab929984c2ca3ff760171a36f19910
VarinCode <warinsaipanya471@outlook.co.th>
2023-10-31 18:58:40 +0700

    อัปเดต

---
+++ data/user.py
@@ -0,0 +1,13 @@
+[{'name': 'john', 'email': 'john@email.com', 'password': '111', 'position': 'ผู้จัดการ'},
+{'name': 'david', 'email': 'david@email.com', 'password': '222', 'position': 'ผู้ดูแลระบบ'},
+{'name': 'jane', 'email': 'jane@email.com', 'password': '333', 'position': 'แคชเชียร์'},
+{'name': 'jack', 'email': 'jack@email.com', 'password': '444', 'position': 'แคชเชียร์'},
+{'name': 'elizabeth', 'email': 'elizabeth@email.com', 'password': '555', 'position': 'พนักงานบาร์'},
+{'name': 'robert', 'email': 'robert@email.com', 'password': '666', 'position': 'ซอมเมลิเยร์'},
+{'name': 'jill', 'email': 'jill@email.com', 'password': '777', 'position': 'ผู้จัดการครัว'},
+{'name': 'jame', 'email': 'james@email.com', 'password': '888', 'position': 'กุ๊ก'},
+{'name': 'william', 'email': 'william@email.com', 'password': '999', 'position': 'ผู้ช่วยกุ๊ก'},
+{'name': 'linda', 'email': 'linda@email.com', 'password': '123', 'position': 'บริกร'},
+{'name': 'jennifer', 'email': 'jennifer@email.com', 'password': '456', 'position': 'พนักงานต้อนรับ'},
+{'name': 'mary', 'email': 'mary@email.com', 'password': '695', 'position': 'พนักงานต้อนรับ'},
+]
